2aad8b4f79bcb05a3546695b5810d29515c6ceff
iBlurBlur <tanakorn.ngam@gmail.com>
2022-09-26 10:41:45 +0700

    Using view components

---
+++ ViewComponents/LoginMenu.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApp.ViewComponents;
+
+public class LoginMenu : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var data = new { name = "tanakorn" };
+        return View(data);
+    }
+}
